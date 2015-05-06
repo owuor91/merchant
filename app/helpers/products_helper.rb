c2067514542219b3c774ca7f857954e514b1767a
owuor91 <owuor91@gmail.com>
2015-05-06 19:55:25 +0300

    Stock tracking added

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -2,4 +2,12 @@ module ProductsHelper
 	def print_price(price)
 		number_to_currency price 
 	end
+
+	def print_stock(stock)
+		if stock > 0
+			content_tag(:span, "#{stock} units remaining" ,class: "in_stock")
+		else
+			content_tag(:span, "Out of stock", class: "out_stock")
+		end
+	end
 end
